e6a738329f0bc7615067ac1edcd2c8f02b94e3a9
Anusan <kailaianu21@gmail.com>
2022-02-10 20:09:22 +0530

    Coded smart contract

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -15,7 +15,7 @@ contract KBMarket is ReentrancyGuard {
      arrays need to know the length - help to keep track for arrays*/
 
     Counters.Counter private _tokenIds;
-    Counters.Counter private _tokenSold;
+    Counters.Counter private _tokensSold;
 
     // determine who is the owner of the contract 
     // charge listing fee so the owner makes a commission
@@ -98,10 +98,56 @@ contract KBMarket is ReentrancyGuard {
     );
 
     // NFT transaction
+    IERC721(nftContract).transferFrom(msg.sender, address(this), tokenId);
+
+    emit MarketTokenMinted(
+          itemId,
+          nftContract,
+          tokenId,
+          msg.sender,
+          address(0),
+          price,
+          false
+     );
+   }
+     // function to coduct transactions and market sales
+
+     function createMarketSale(
+         address nftContract,
+         uint itemId)
+         public payable nonReentrant {
+             uint price = idToMarketToken[itemId].price;
+             uint tokenId = idToMarketToken[itemId].tokenId;
+             require(msg.value == price, 'Please submit the askng price in order to continue');
+
+             // transfer the amount of the seller
+             idToMarketToken[itemId].seller.transfer(msg.value);
+             // transfer the token from contract address to the buyer
+             IERC721(nftContract).transferFrom(address(this), msg.sender, tokenId);
+             idToMarketToken[itemId].owner = payable(msg.sender);
+             idToMarketToken[itemId].sold = true;
+             _tokenSold.increment();
+
+             payable(owner).transfer(listingPrice);
+
+         }
+     
+
+     // function to fetchMarketItems - minting, buying and selling
+     // return the number of unsold items
+
+     function fetchMarketTokens() public view returns(MarketToken[] memory) {
+         uint itemCount = _tokenIds.current();
+         uint unsoldItemCount = _tokenIds.current() - _tokensSold.current();
+         uint currentIndex = 0;
+
+         // looping ovr the number of items created (if number has not been sold populate the array)
+         MarketToken[] memory items = new MarketToken[](unsoldItemCount); 
+     }
 
    }
 
 
 
-}
+
 
