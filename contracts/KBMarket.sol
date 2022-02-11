567ebc73920991a6c7a46c735a34aa7f1b69983f
Anusan <kailaianu21@gmail.com>
2022-02-11 12:20:33 +0530

    return the number of unsold items

--- contracts/KBMarket.sol
+++ contracts/KBMarket.sol
@@ -143,8 +143,26 @@ contract KBMarket is ReentrancyGuard {
 
          // looping ovr the number of items created (if number has not been sold populate the array)
          MarketToken[] memory items = new MarketToken[](unsoldItemCount); 
+         for(uint i = 0; i < itemCount; i++) {
+             if(idToMarketToken[i + 1].owner == address(0)) {
+             uint currentId = i + 1;
+             MarketToken storage currentItem = idToMarketToken[currentId];
+             items[currentIndex] = currentItem;
+             currentIndex += 1;
+         }
+     }
+     return items;
+
      }
 
+     // return nfts that the user has purchased
+
+     function fetchMyNFTs() public view returns (MarketToken[] memory) {
+         uint totalItemCount = _tokenIds.current();
+         // a second counter for each indiviual user
+         uint itemCount = 0;
+         uint currentIndex = 0;
+     }
    }
 
 
