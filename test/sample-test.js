81f34fc037e43b4bd1c4d90455331e055ea724bb
Anusan <kailaianu21@gmail.com>
2022-02-14 00:08:59 +0530

    run test

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,10 +3,45 @@ const { ethers } = require("hardhat");
 
 describe("KBMarket", function () {
   it("Should mint and tradde NFTs", async function () {
+
+    // test to receive contract addresses
     const Market = await ethers.getContractFactory('KBMarket')
     const market = await Market.deploy()
     await market.deployed()
     const marketAddress = market.address
+
+    const NFT = await ethers.getContractFactory('NFT')
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
+
+
+    // test to receive listing price and auction price
+    let listingPrice = await market.getListingPrice()
+    listingPrice = listingPrice.toString()
+
+    const auctionPrice = ethers.utils.parseUnits('100','ether')
+
+    // test for minting
+    await nft.mintToken('https-t1')
+    await nft.mintToken('https-t2')
+
+    await market.makeMarketItem(nftContractAddress, 1, auctionPrice, {value: listingPrice})
+    await market.makeMarketItem(nftContractAddress, 2, auctionPrice, {value: listingPrice})
+
+    // test for different addresses from different users - test accounts
+    // return an array of however many addresses
+    const [_, buyerAddress] = await ethers.getSigners()
+
+    // create a market sale with address, id and price
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, {
+      value: auctionPrice
+    })
+
+    const items = await market.fetchMarketTokens()
+
+    // test out all the items
+    console.log('items', items)
 
   });
 });
