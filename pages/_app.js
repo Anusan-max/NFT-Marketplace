4dbd386bebda58d93509d3050fa87e2bd668252c
Anusan <kailaianu21@gmail.com>
2022-03-28 16:53:25 +0530

    made changes in the design

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import Link from 'next/link'
 function KryptoBirdMarketplace({Component, pageProps}) {
   return (
     <div>
-      <nav className='border-b p-6' style={{backgroundColor:'purple'}}>
-        <p className='text-4x1 font-bold text-white'>KryptoBird Marketplace</p>
+      <nav className='border-b p-6' style={{backgroundColor:'orange'}}>
+        <p className='text-4x1 font-bold text-white'>NFT Marketplace</p>
         <div className='flex mt-4 justify-center'>
           <Link href='/'>
             <a className='mr-4'>
